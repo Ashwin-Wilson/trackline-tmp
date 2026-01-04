2e7516a9d53ab78d7c5276fdd18249b10ffd3855
Ashwin-Wilson <ashwinwilson24@gmail.com>
2026-01-04 19:00:33 +0530

    fix(cors): enabled all domains and methods to access api's usign *

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"]
+)
 
 @app.get("/")
 def greetings():
     return "Hello world"
+
